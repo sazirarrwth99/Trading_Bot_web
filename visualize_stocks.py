9ce10980f3eb95f786cf6f701ed55363170ef5a5
sazirarrwth99 <116290380+sazirarrwth99@users.noreply.github.com>
2023-01-12 23:20:28 +0100

    Update visualize_stocks.py

--- visualize_stocks.py
+++ visualize_stocks.py
@@ -21,7 +21,7 @@ from agent import *
 from enviroment import *
 import os
 
-enviroment = Enviroment("/Users/samiazirar/FDS_Trading_Bot/Reinforcement_learning/AAPL_daily_yf_test.csv")
+enviroment = Enviroment("/AAPL_daily_yf_test.csv")
 enviroment.data_indicator = ".csv"
 #enviroment.append_new_data("Close_Minute/")
 enviroment.get_data()
