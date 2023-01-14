fac6597ca6807a2cdc056a3197c132e5f5b14413
sazirarrwth99 <116290380+sazirarrwth99@users.noreply.github.com>
2023-01-14 15:19:48 +0100

    Update visualize_stocks.py

--- visualize_stocks.py
+++ visualize_stocks.py
@@ -43,9 +43,9 @@ while True:
         cash_ = 10000 # set a default symbol
     # Get the stock data
     try:
-        start_time = dt.datetime.now() - dt.timedelta(days=3)
+        start_time = dt.datetime.now() - dt.timedelta(days=5)
         while stock_data["Close"].max() < 10:
-            stock_data = yf.download(stock_symbol, start=start_time, interval="1m",period="3d")
+            stock_data = yf.download(stock_symbol, start=start_time, interval="1m",period="5d")
         timeseries = list(stock_data["Close"].values)
         list_actions = [ enviroment.predict(timeseries, t=i) for i in tqdm(range(1023, len(timeseries)))]
         #generate profits
